84280204ac4c36aa85cbb8458ebf968970c53876
LucasGFT <lucasgft200303@gmail.com>
2023-06-20 16:04:12 -0300

    criacao da model respostas

--- model/questao.ts
+++ model/questao.ts
@@ -1,11 +1,12 @@
+import RespostaModel from "./resposta";
+
 export default class QuestaoModel {
     #id: number;
     #enunciado: string;
-    #respostas: any[];
+    #respostas: RespostaModel[];
     #acertou: boolean;
-    // #respondida: boolean;
 
-    constructor(id: number, enunciado: string, respostas: any[], acertou = false) {
+    constructor(id: number, enunciado: string, respostas: RespostaModel[], acertou = false) {
         this.#id = id;
         this.#enunciado = enunciado;
         this.#respostas = respostas;
@@ -29,6 +30,10 @@ export default class QuestaoModel {
     }
 
     get respondida() {
-        return false
+        for(let resposta of this.#respostas) {
+            if(resposta.revelada) return true;
+        }
+        return false 
     }
+    
 }

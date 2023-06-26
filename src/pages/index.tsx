5f88afec4ee4a0d4d9fa54911585667eaa919c6f
LucasGFT <lucasgft200303@gmail.com>
2023-06-26 13:59:54 -0300

    apagando barra na url de deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import QuestaoModel from "@/model/questao";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-const BASE_URL = 'https://quiz-lucasgft.vercel.app/'
+const BASE_URL = 'https://quiz-lucasgft.vercel.app'
 
 export default function Home() {
   const router = useRouter()

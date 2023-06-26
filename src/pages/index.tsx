40cb171a725fb42fb9ad70b8114b87618604a962
LucasGFT <lucasgft200303@gmail.com>
2023-06-26 13:59:20 -0300

    arrumando link de deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import QuestaoModel from "@/model/questao";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = 'https://quiz-lucasgft.vercel.app/'
 
 export default function Home() {
   const router = useRouter()
